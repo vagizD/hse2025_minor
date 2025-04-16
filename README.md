8f1d44239fdccf6c5257142fb3655c90153ae557
Vagiz Daudov <vagiz.0282@gmail.com>
2025-04-17 00:01:33 +0300

    Add instruction

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 
 # HSE 2025 Minor
+
+```bash
+pip install -r requirements.txt
+python3 app.py
+```
--- app.py
+++ app.py
@@ -17,8 +17,8 @@ def main():
 
     # create migrations if applicable and run them
     commit_message = "INIT" if is_recreated else "AUTO_GENERATED_COMMIT"
-    # subprocess.run(['alembic', 'revision', '--autogenerate', '-m', f'"{commit_message}"'])
-    subprocess.run(['alembic', 'upgrade', 'head'])
+    # subprocess.run(['alembic', 'revision', '--autogenerate', '-m', f'"{commit_message}"'])  # new revision
+    subprocess.run(['alembic', 'upgrade', 'head'])                                            # upgrade head
 
     # insert initial data
     if is_recreated:
